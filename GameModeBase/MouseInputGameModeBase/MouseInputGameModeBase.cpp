de3f633fdb20e3ec9ef0c70901e2b74defcb0a1c
ironscar <uchakravarty29@gmail.com>
2023-06-17 08:37:25 +0530

    fix: mouse input game mode bp path

--- GameModeBase/MouseInputGameModeBase/MouseInputGameModeBase.cpp
+++ GameModeBase/MouseInputGameModeBase/MouseInputGameModeBase.cpp
@@ -1,14 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-
 #include "MouseInputGameModeBase.h"
 
 AMouseInputGameModeBase::AMouseInputGameModeBase() {
 	// set pawn
-	ConstructorHelpers::FObjectFinder<UClass> PawnClass(TEXT("Class'/Game/LearningKit_Games/Blueprints/MyClasses/BP_MyCharacterPawn.BP_MyCharacterPawn_C'"));
+	ConstructorHelpers::FObjectFinder<UClass> 
+		PawnClass(TEXT("Class'/Game/LearningKit_Games/Blueprints/MyClasses/Characters/Common/BP_MyCharacterPawn.BP_MyCharacterPawn_C'"));
 	DefaultPawnClass = PawnClass.Object != nullptr ? PawnClass.Object : nullptr;
 
 	// set player controller
-	ConstructorHelpers::FObjectFinder<UClass> ControllerClass(TEXT("Class'/Game/LearningKit_Games/Blueprints/MyClasses/BP_MyCharacterPlayerController.BP_MyCharacterPlayerController_C'"));
+	ConstructorHelpers::FObjectFinder<UClass> 
+		ControllerClass(TEXT("Class'/Game/LearningKit_Games/Blueprints/MyClasses/Characters/Common/BP_MyCharacterPlayerController.BP_MyCharacterPlayerController_C'"));
 	PlayerControllerClass = ControllerClass.Object != nullptr ? ControllerClass.Object : nullptr;
 }
